b6c7f4a2ad26514319239417011d7bf6f7c28148
Daniel Marriner <danielmarriner@anachronistic-tech.co.uk>
2021-10-19 20:40:01 +0300

    Created `String.kebabCase()` extension

--- Sources/DatapackKit/Utilities/KebabCase.swift
+++ Sources/DatapackKit/Utilities/KebabCase.swift
@@ -1,19 +1,18 @@
 @propertyWrapper struct KebabCase {
     var wrappedValue: String {
         didSet {
-            wrappedValue = wrappedValue
-                .replacingOccurrences(
-                    of: "([A-Z])",
-                    with: "-$1",
-                    options: .regularExpression
-                )
-                .lowercased()
+            wrappedValue = wrappedValue.kebabCase()
         }
     }
 
     init(wrappedValue: String) {
-        self.wrappedValue = wrappedValue
-            .replacingOccurrences(
+        self.wrappedValue = wrappedValue.kebabCase()
+    }
+}
+
+extension String {
+    func kebabCase() -> String {
+        self.replacingOccurrences(
                 of: "([A-Z])",
                 with: "-$1",
                 options: .regularExpression

53172c6cca0405b052cf949a7f239193b5a17e29
Daniel Marriner <danielmarriner@anachronistic-tech.co.uk>
2021-10-26 22:20:49 +0300

    Added entity selector unit tests

---
+++ Tests/DatapackKitTests/EntitySelectorTests.swift
@@ -0,0 +1,43 @@
+import XCTest
+import class Foundation.Bundle
+import struct Foundation.UUID
+@testable import DatapackKit
+
+final class EntitySelectorTests: XCTestCase {
+    func testNearestPlayerOutput() throws {
+        let selector = EntitySelector.nearestPlayer
+        XCTAssertEqual("\(selector)", "@p")
+    }
+
+    func testRandomPlayerOutput() throws {
+        let selector = EntitySelector.randomPlayer
+        XCTAssertEqual("\(selector)", "@r")
+    }
+
+    func testAllPlayersOutput() throws {
+        let selector = EntitySelector.allPlayers
+        XCTAssertEqual("\(selector)", "@a")
+    }
+
+    func testAllEntitiesOutput() throws {
+        let selector = EntitySelector.allEntites
+        XCTAssertEqual("\(selector)", "@e")
+    }
+
+    func testExecutorOutput() throws {
+        let selector = EntitySelector.executor
+        XCTAssertEqual("\(selector)", "@s")
+    }
+
+    func testNamedPlayerOutput() throws {
+        let name = "Steve"
+        let selector = EntitySelector.namedPlayer(name)
+        XCTAssertEqual("\(selector)", name)
+    }
+
+    func testUUIDOutput() throws {
+        let uuid = UUID()
+        let selector = EntitySelector.uuid(uuid)
+        XCTAssertEqual("\(selector)", "\(uuid)")
+    }
+}
